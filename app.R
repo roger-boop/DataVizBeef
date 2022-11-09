d08136df8ba84396265d338f22b18f668cef6b5a
sampledrift <95143095+sampledrift@users.noreply.github.com>
2022-11-09 12:49:36 +0100

    Add files via upload

--- app.R
+++ app.R
@@ -28,6 +28,12 @@ ui <- fluidPage(
     
     sidebarLayout(
         sidebarPanel(
+          #heatmap variables
+          checkboxGroupInput("heatmapModeInput", "Continents", choices = unique(DATA$Mode), selected = unique(DATA$Mode)),
+          checkboxGroupInput("heatmapModernPopInput", "Modern Populations", choices = unique(DATA$Modern_pop), selected = unique(DATA$Modern_pop)),
+          checkboxGroupInput("heatmapArchaicPopInput", "Archaic Populations", choices = unique(DATA$Archaic_pop), selected = unique(DATA$Archaic_pop)),
+          #violin variables
+          br(), br(),
           sliderInput("violinUniqSharedInput", "Unique Shared", min = min(DATA$Uniq_Shared), 
                       max = max(DATA$Uniq_Shared), value = c(min(DATA$Uniq_Shared), max(DATA$Uniq_Shared))),
             checkboxGroupInput("violinModernPopInput", "Modern Populations", choices = unique(DATA$Modern_pop), selected = unique(DATA$Modern_pop)),
@@ -35,15 +41,50 @@ ui <- fluidPage(
         ),
         
         mainPanel(
-           plotOutput("violinPlot"),
-           dataTableOutput("violinResults")
+          #histogram
+          #plotOutput("histogramPlot"),
+          #dataTableOutput("histogramResults")
+          br(), br(),
+          #heatmap
+          plotOutput("heatmapPlot"),
+          dataTableOutput("heatmapResults"),
+          br(), br(),
+          #violin plot
+          plotOutput("violinPlot"),
+          dataTableOutput("violinResults")
         )
     )
 )
 
 server <- function(input, output) {
+  
+    # HEATMAP PLOT
+    
+    output$heatmapPlot <- renderPlot({
+      heatmapFiltered <- DATA %>% filter(
+        Mode %in% input$heatmapModeInput,
+        Modern_pop %in% input$heatmapModernPopInput,
+        Archaic_pop %in% input$heatmapArchaicPopInput
+      )
+      
+      ggplot(data = heatmapFiltered, aes(x = Archaic_pop, y = Modern_pop)) + geom_bin2d() + 
+        facet_grid(cols = vars(Mode), scales = "free") + theme(panel.grid.major.x = element_blank()) + 
+        xlab("Archaic population") + ylab("Modern population") + scale_fill_viridis("Amount")
+    })
+    
+    output$heatmapResults <- renderDataTable({
+      heatmapFiltered <- DATA %>% filter(
+        Mode %in% input$heatmapModeInput,
+        Modern_pop %in% input$heatmapModernPopInput,
+        Archaic_pop %in% input$heatmapArchaicPopInput
+      )
+      heatmapFiltered
+    })
+    
+    # VIOLIN PLOT
+  
     output$violinPlot <- renderPlot({
-      filtered <- DATA %>% filter(
+      violinFiltered <- DATA %>% filter(
         Uniq_Shared >= input$violinUniqSharedInput[1],
         Uniq_Shared <= input$violinUniqSharedInput[2],
         Modern_pop %in% input$violinModernPopInput,
@@ -51,19 +92,19 @@ server <- function(input, output) {
         
       )
       # FIX: ARCHAIC LABELS BEING ALWAYS NEA
-      ggplot(data = filtered, mapping = aes(x = Modern_pop, y = Uniq_Shared)) + geom_jitter(mapping = aes(colour = Archaic_pop)) + 
-        geom_violin(alpha = 0.4) + scale_colour_viridis_d("Archaic Population", labels = filtered$Archaic_pop) + 
+      ggplot(data = violinFiltered, mapping = aes(x = Modern_pop, y = Uniq_Shared)) + geom_jitter(mapping = aes(colour = Archaic_pop)) + 
+        geom_violin(alpha = 0.4) + scale_colour_viridis_d("Archaic Population", labels = violinFiltered$Archaic_pop) + 
         labs(xlab = "Modern population", ylab = "Uniquely shared", title = "Uniquely shared alleles in modern populations") + 
         theme_classic() + theme(axis.text.x = element_text(angle = 45, hjust = 1))
     })
     output$violinResults <- renderDataTable({
-      filtered <- DATA %>% filter(
+      violinFiltered <- DATA %>% filter(
         Uniq_Shared >= input$violinUniqSharedInput[1],
         Uniq_Shared <= input$violinUniqSharedInput[2],
         Modern_pop %in% input$violinModernPopInput,
         Archaic_pop %in% input$violinArchaicPopInput
       )
-      filtered
+      violinFiltered
     })
 }
 
